6688c9f37d3717beeb89297f5ed51ce214aed544
Victor Schmücker Serpa <schmvitoor@gmail.com>
2025-02-13 09:31:41 -0300

    chore: ajustes nos testes e integração contínua no GitHub Actions

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+    NEXT_PUBLIC_VIA_CEP_URl: process.env.NEXT_PUBLIC_VIA_CEP_URl,
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
